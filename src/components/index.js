7621fc86d9416cb84f388ff8cc6f469e87493e58
MarvinMLopez <86867050+MarvinMLopez@users.noreply.github.com>
2021-07-14 11:28:56 -0600

    se agregó las rutas del historial del vendedor

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Home from './home/';
 import Login from './login/';
 import Registro from './registro/';
 import Catalogo from './catalogo/';
+import HistorialVendedor from './HistorialVendedor/';
 
 export {
     Navigation,
     Home,
     Login,
     Registro,
-    Catalogo
+    Catalogo,
+    HistorialVendedor
 }
