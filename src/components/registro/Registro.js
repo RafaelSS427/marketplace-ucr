f42f1f8004a6d013c175e8a23c9c8a342d3c80a1
MarvinMLopez <86867050+MarvinMLopez@users.noreply.github.com>
2021-07-14 15:15:20 -0600

    se agregó unos campos simples

--- src/components/registro/Registro.js
+++ src/components/registro/Registro.js
@@ -1,9 +1,43 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Registro = () => {
 
     return(
-        <h1>Estamos en registro</h1>
+<>
+      <div className="container mt-lg-5">
+          <div className="row d-flex justify-content-center">
+            <div className="col-4 col-md-4">
+              <div className="card">
+                <div className="card-header text-center">
+                  <h4>Registro</h4>
+                </div>
+                <div className="card-body">
+                <div className="mb-3">
+                      <input type="nombre" className="form-control"  placeholder="Ingrese Nombre Completo" />  
+                  </div>
+                  <div className="mb-3">
+                      <input type="nombreEmp" className="form-control"  placeholder="Ingrese Nombre de su Empresa" />  
+                  </div>
+                  <div className="mb-3">
+                      <input type="telefono" className="form-control"  placeholder="Ingrese Telefono" />  
+                  </div>
+
+                  <div className="mb-3">
+                      <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Ingrese su correo electronico" />  
+                  </div>
+                    
+                  <div className="mb-3">
+                    <input type="password" className="form-control" id="password" placeholder="Ingrese su Contraseña" />
+                  </div>
+                        
+                  <Link to="/HistorialVendedor" className="btn btn-primary w-100">Registrarse</Link>  
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      </>
     )
 
 }
